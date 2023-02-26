8d7b5429aaee2efa1928bd299a552148a259dfbd
Nya Meteor <26038696+nyameteor@users.noreply.github.com>
2023-02-26 21:10:57 +0800

    done with emptySpaceExists, maxTileExists, atLeastOneMoveExists

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -137,7 +137,14 @@ public class Model extends Observable {
      *  Empty spaces are stored as null.
      * */
     public static boolean emptySpaceExists(Board b) {
-        // TODO: Fill in this function.
+        int size = b.size();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (b.tile(i, j) == null) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -147,7 +154,15 @@ public class Model extends Observable {
      * given a Tile object t, we get its value with t.value().
      */
     public static boolean maxTileExists(Board b) {
-        // TODO: Fill in this function.
+        int size = b.size();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                Tile t = b.tile(i, j);
+                if (t != null && t.value() == MAX_PIECE) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -158,7 +173,26 @@ public class Model extends Observable {
      * 2. There are two adjacent tiles with the same value.
      */
     public static boolean atLeastOneMoveExists(Board b) {
-        // TODO: Fill in this function.
+        if (emptySpaceExists(b)) {
+            return true;
+        }
+        int size = b.size();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                Tile t = b.tile(i, j);
+                Tile rt = i + 1 < size ? b.tile(i + 1, j) : null;
+                Tile dt = j + 1 < size ? b.tile(i, j + 1) : null;
+                if (t == null) {
+                    continue;
+                }
+                if (rt != null && t.value() == rt.value()) {
+                    return true;
+                }
+                if (dt != null && t.value() == dt.value()) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
