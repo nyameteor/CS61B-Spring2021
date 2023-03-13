832e0b479f93a508923725685bd2159840114b55
Nya Meteor <26038696+nyameteor@users.noreply.github.com>
2023-03-14 07:34:14 +0800

    done with GuitarHero

---
+++ proj1/gh2/GuitarHero.java
@@ -0,0 +1,43 @@
+package gh2;
+
+import edu.princeton.cs.algs4.StdAudio;
+import edu.princeton.cs.algs4.StdDraw;
+
+/**
+ * Similar to GuitarHeroLite, but supports a total of 37 notes on the chromatic scale
+ * from 110Hz to 880Hz.
+ */
+public class GuitarHero {
+    private static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+    private static final double CONCERT_A = 440.0;
+
+    public static void main(String[] args) {
+        GuitarString[] strings = new GuitarString[keyboard.length()];
+        for (int i = 0; i < strings.length; i++) {
+            double frequency = CONCERT_A * Math.pow(2, (i - 24.0) / 12.0);
+            strings[i] = new GuitarString(frequency);
+        }
+
+        while (true) {
+            if (StdDraw.hasNextKeyTyped()) {
+                char key = StdDraw.nextKeyTyped();
+                int i = keyboard.indexOf(key);
+                if (i == -1) {
+                    continue;
+                }
+                strings[i].pluck();
+            }
+
+            double sample = 0.0;
+            for (GuitarString string : strings) {
+                sample += string.sample();
+            }
+
+            StdAudio.play(sample);
+
+            for (GuitarString string : strings) {
+                string.tic();
+            }
+        }
+    }
+}
